f722a47f990569db7d6c547338a472fa00b7be2b
Shelsy Garcia <shelsyangelik@gmail.com>
2023-05-16 18:46:35 -0500

    img

--- codigo.js
+++ codigo.js
@@ -4,3 +4,17 @@ const menu = document.querySelector('.menu ul');
 menuToggle.addEventListener('click', function() {
   menu.classList.toggle('show');
 });
+
+const donacionMonetaria = document.querySelector('#donacion-monetaria');
+const imgMediosPago = document.querySelector('#img-medios-pago');
+
+donacionMonetaria.addEventListener('click', function(){
+  imgMediosPago.classList.toggle('show')
+})
+
+const donacionEspecie = document.querySelector('#donacion-especie');
+const imgProductos = document.querySelector('#img-productos');
+
+donacionEspecie.addEventListener('click', function(){
+  imgProductos.classList.toggle('show')
+})
